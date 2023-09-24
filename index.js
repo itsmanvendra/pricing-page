8d7cfe50ca6d7fca6f4eaf7db4101b56bc2a7c36
itsmanvendra <singh03manvendra@gmail.com>
2023-09-24 15:45:15 +0530

    done

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-const range = document.querySelector("#customRange3");
+const customerRange = document.querySelector("#numberOfCustomers");
 const free = document.querySelector("#free");
 const pro = document.querySelector("#pro");
 const enterprise = document.querySelector("#enterprise");
 const submitButton = document.querySelector("#submit");
-const form = document.querySelector(".modal-body")
+const form = document.querySelector("form");
 
-    
-range.addEventListener("change", (e) => {
-    console.log(e.target.value);
-    if (e.target.value >= 0 && e.target.value <= 10) {
-        enterprise.toggleAttribute("checked", false);
-        pro.toggleAttribute("checked", false);
-        free.toggleAttribute("checked", true);
-    }
-    else if (e.target.value >= 11 && e.target.value <= 20) {
-        free.toggleAttribute("checked", false);
-        enterprise.toggleAttribute("checked", false);
-        pro.toggleAttribute("checked", true);
-    }
-    else if (e.target.value >= 21 && e.target.value <= 30) {
-        pro.toggleAttribute("checked", false);
-        free.toggleAttribute("checked", false);
-        enterprise.toggleAttribute("checked", true);
-    }
+customerRange.addEventListener("change", (e) => {
+  document.querySelector(".range-slider__value").innerHTML = e.target.value;
+  if (e.target.value >= 0 && e.target.value <= 10) {
+    enterprise.toggleAttribute("checked", false);
+    pro.toggleAttribute("checked", false);
+    free.toggleAttribute("checked", true);
+  } else if (e.target.value >= 11 && e.target.value <= 20) {
+    free.toggleAttribute("checked", false);
+    enterprise.toggleAttribute("checked", false);
+    pro.toggleAttribute("checked", true);
+  } else if (e.target.value >= 21 && e.target.value <= 30) {
+    pro.toggleAttribute("checked", false);
+    free.toggleAttribute("checked", false);
+    enterprise.toggleAttribute("checked", true);
+  }
 });
+
+async function sendData(data) {
+  const response = await fetch("http://localhost:3000/proxy", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  const json = await response.json();
+  console.log(json);
+  document.querySelector("#formModal").classList.toggle("show");
+  document.querySelector(".modal-backdrop").classList.toggle("show");
+}
 
 submitButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    console.log(data);
-    const values = [...formData.entries()];
-    console.log(values);
-    fetch("https://forms.maakeetoo.com/formapi/791", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        // "Access-Control-Allow-Origin": "*",
-        // "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-        AccessCode: "VSCE8U566X7ETPTFNCTN1HAKP",
-      },
-    });
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  sendData(data);
 });
+
 
